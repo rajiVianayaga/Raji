c482bd0d137048ff78c171640bab67ab796fa1cb
rajiVinayaga <rajideivanayagi@gmail.com>
2021-10-08 15:16:32 +0530

    username functionality

--- src/test/java/org/maven/GitMaven.java
+++ src/test/java/org/maven/GitMaven.java
@@ -1,5 +1,13 @@
 package org.maven;
 
 public class GitMaven {
-
+	public static void username()
+	{
+		System.out.println("Username functionality");
+	}
+	public static void main(String[] args)
+{
+	GitMaven g = new GitMaven();
+	g.username();
+}
 }
