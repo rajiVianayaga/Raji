c28084597550d818010cff1df830091f71f3b880
rajiVinayaga <rajideivanayagi@gmail.com>
2021-10-08 15:22:39 +0530

    password functionality

--- src/test/java/org/maven/GitMaven.java
+++ src/test/java/org/maven/GitMaven.java
@@ -1,5 +1,12 @@
 package org.maven;
 
 public class GitMaven {
-
+	public void password()
+	{
+		System.out.println("Password Functionality");
+	}
+	public static void main(String[] args) {
+		GitMaven g = new GitMaven();
+		g.password();
+	}
 }
